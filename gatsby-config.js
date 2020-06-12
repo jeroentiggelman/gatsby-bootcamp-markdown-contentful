533a1377cad20f47b91ef73cc4e3641bf898ca7b
jeroentiggelman <infos@photographicdesign.be>
2020-06-12 18:29:42 +0200

    added dynamic site title and author via graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,10 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Gatsby tutorial",
+    author: "Jeroen Tiggelman",
+  },
   /* Your site config here */
   plugins: [`gatsby-plugin-sass`],
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,24 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <footer>
-      <p>Created by Jeroen Tiggelman, ©2020</p>
+      <p>
+        <strong>{data.site.siteMetadata.title}</strong> created by{" "}
+        {data.site.siteMetadata.author}, © 2020
+      </p>
     </footer>
   )
 }
