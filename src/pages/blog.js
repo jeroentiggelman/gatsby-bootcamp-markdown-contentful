5c7bd5288bafa5263ed794cec43262fd15637f63
jeroentiggelman <infos@photographicdesign.be>
2020-06-12 23:11:09 +0200

    query md files, generate post list on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,39 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  // put data array in 1 variable (not obligatory, more to keep code easier to read)
+  const posts = data.allMarkdownRemark.edges
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here.</p>
+      <ol>
+        {/* map posts from array with loop */}
+        {posts.map(post => {
+          return (
+            <li>
+              <h2>{post.node.frontmatter.title}</h2>
+              <p>{post.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
